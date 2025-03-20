6a2ce9d7180ca56533c9dc1afebddba2f9ea1d69
Aaron P Johnson <aaron.p.johnson91@gmail.com>
2025-03-20 16:11:56 -0600

    Update server.js

--- services/template/src/server.js
+++ services/template/src/server.js
@@ -1,1 +1,4 @@
-
+const express = require('express');
+const app = express();
+app.get('/api/health', (req, res) => res.json({ status: 'ok' }));
+app.listen(8080, () => console.log('Server on port 8080'));
